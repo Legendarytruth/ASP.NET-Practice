df1704ec7a39b7fe8907a398204bfe3635eb9028
Legendarytruth <arthur.sit99@gmail.com>
2021-10-22 02:33:38 -0400

    3. Controllers
    For Essential Training

---
+++ ASP.NET MVC 5 Essential Training/HPlus.Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPlus.Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View("List");
+        }
+        
+        [Route("product/{productName}")]
+        public ActionResult Detail(string productName)
+        {
+            return View();
+        }
+    }
+}
