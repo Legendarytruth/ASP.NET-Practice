6f1d977c5b10f1709474ef71ceeae17baf7bb0f5
Legendarytruth <arthur.sit99@gmail.com>
2021-11-13 18:53:21 -0500

    4. Security

---
+++ ASP.NET MVC 5 Authentication and Authorization/Security/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using Security.Models;
+using System.Web.Mvc;
+
+namespace Security.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create([Bind(Include = "Name, Description")]Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(department);
+        }
+    }
+}
---
+++ ASP.NET MVC 5 Authentication and Authorization/Security/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Security.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Professor Professor { get; set; }
+    }
+}
