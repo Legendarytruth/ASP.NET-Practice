8ab39f0d57e02d64d72823703556e6369803a6c1
Legendarytruth <arthur.sit99@gmail.com>
2021-11-13 18:06:30 -0500

    3. Authorization

--- ASP.NET MVC 5 Authentication and Authorization/SocialIdentity/Controllers/HomeController.cs
+++ ASP.NET MVC 5 Authentication and Authorization/SocialIdentity/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialIdentity.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +7,33 @@ using System.Web.Mvc;
 
 namespace SocialIdentity.Controllers
 {
+    [Authorize(Roles = "Administrators, Managers")]
     public class HomeController : Controller
     {
+        [Authorize(Roles = "Managers")]
         public ActionResult Index()
         {
             return View();
         }
 
+        //[AllowAnonymous]
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            ViewBag.Message = "Your contact page.";
+            var VM = new MyViewModel();
+            VM.IsAdmin = false;
+            return View(VM);
         }
 
+        [Authorize(Roles = "Admin")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
-            return View();
+            var VM = new MyViewModel();
+            VM.IsAdmin = true;
+            return View(VM);
         }
     }
 }
